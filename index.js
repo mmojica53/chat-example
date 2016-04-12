1572ecd96c75616a816f491f7b9af3164d32d864
Marco Mojica <mmojica53@users.noreply.github.com>
2016-04-12 16:00:32 -0400

    Update index.js
    Intentional misconfiguration to see PMX behavior.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var app = require('express')();
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+//app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
